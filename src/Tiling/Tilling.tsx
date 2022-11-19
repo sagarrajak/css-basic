6b596341c10b2fe37e843339d4d208584696c105
Sagar <sagarrajak858@gmail.com>
2022-11-20 02:46:26 +0530

    grid commit

---
+++ src/Tiling/Tilling.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Tiling.css';
+
+const Tilling = () => {
+    return (
+        <div className="container">
+            <div className="el1">Element 1</div>
+            <div className="el2">Element 2</div>
+            <div className="el3">Element 3</div>
+            <div className="el4">Element 4</div>
+        </div>
+    )
+}
+
+export default Tilling
---
+++ src/modules/grid/Grid.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Grid.css';
+
+function Grid() {
+    return (
+        <div className='grid-container'>
+            <div className='header'>header</div>
+            <div className="sidebar">Sidebar</div>
+            <div className="main-content">Main Content</div>
+            <div className="footer">Footer</div>           
+        </div>
+    )
+}
+
+export default Grid;
